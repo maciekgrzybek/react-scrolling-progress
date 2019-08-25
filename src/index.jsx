37aee7e29cb3e0519db4131c219f014bdeab98e0
Maciek Grzybek <maciekgrzybek1@gmail.com>
2019-08-25 18:40:55 +0100

    Update example

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,13 @@ import ScrollProgress from './components/scroll-progress';
 const root = document.getElementById('root');
 
 render(
-  (
+  ( 
+    <div style={{
+      height: 3000,
+      background: 'lightpink',
+    }}>
     <ScrollProgress />
+    </div>
+
   ), root,
 );
