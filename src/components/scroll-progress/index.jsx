907b4a530565ca49d7620afd4c34401d482e7991
Maciek Grzybek <maciekgrzybek1@gmail.com>
2019-08-25 18:13:18 +0100

    Create dynamic styles

--- src/components/scroll-progress/index.jsx
+++ src/components/scroll-progress/index.jsx
@@ -1,22 +1,56 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
-const ScrollProgress = ({styles, position}) => {
 const ScrollProgress = ({styles, position, className, backgroundColor, barSize}) => {
 
   const [progress, setScrollProgress] = useState(0);
   const [viewportHeight, setViewportHeight] = useState(0);
 
+  const setDynamicStyles = () => {
+    let xAxisStyle = {
+      left: 0,
+      right:0,
+      width: progress + '%',
+      height: barSize,
+    };
+    let yAxisStyle = {
+      top: 0,
+      width: barSize,
+      height: progress + '%',
+    }
+    switch (position) {
+      case 'top':
+        return {
+          top: 0,
+          ...xAxisStyle
+        }
+      case 'bottom':
+        return {
+          bottom: 0,
+          ...xAxisStyle
+        }
+      case 'left':
+        return {
+          left: 0,
+          ...yAxisStyle
+        }
+      case 'right':
+        return {
+          right: 0,
+          ...yAxisStyle
+        }
+      default:
+        return;
+    }
+  }
+
   let progressBarStyle = {
-    background: 'red',
+    background: backgroundColor,
     position: 'fixed',
-    top: 0,
-    left: 0,
-    right:0,
-    width: progress + '%',
-    height: 15,
-    WebkitTransition: 'all .2s ease-out',
-    transition: 'all .2s ease-out'
+    transition: 'all .2s ease-out',
+    zIndex: 999,
+    ...setDynamicStyles(),
+    ...styles,
   }
   
   useEffect(() => {
@@ -63,7 +97,7 @@ const ScrollProgress = ({styles, position, className, backgroundColor, barSize})
       background: 'yellow',
     }}
     >
-      <div className="progress-bar" style={ progressBarStyle }></div>
+      <div className={className} style={ progressBarStyle }></div>
     </div>
   );
 };
@@ -71,6 +105,9 @@ const ScrollProgress = ({styles, position, className, backgroundColor, barSize})
 ScrollProgress.defaultProps = {
   styles: {},
   position: 'top',
+  className: 'progress-bar',
+  backgroundColor: 'black',
+  barSize: 45,
 };
 
 ScrollProgress.propTypes = {
@@ -81,6 +118,9 @@ ScrollProgress.propTypes = {
     'left',
     'right',
   ]),
+  className: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  barSize: PropTypes.number,
 };
 
 export default ScrollProgress;
